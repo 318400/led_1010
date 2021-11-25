4c707a077689b3029b29c06a02f5a0f987a9b5f5
318400 <91246976+318400@users.noreply.github.com>
2021-11-25 16:42:36 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,84 @@
+input.onPinPressed(TouchPin.P0, function () {
+    _type = 3
+})
+input.onButtonPressed(Button.A, function () {
+    _type = 2
+})
+input.onButtonPressed(Button.AB, function () {
+    _type = 0
+})
+input.onButtonPressed(Button.B, function () {
+    _type = 1
+})
+input.onPinPressed(TouchPin.P1, function () {
+    _type = 4
+})
+let item: number[] = []
+let _type = 0
+let list = [
+[
+1,
+0,
+0,
+0,
+1
+],
+[
+0,
+1,
+0,
+1,
+0
+],
+[
+1,
+0,
+1,
+0,
+1
+],
+[
+0,
+1,
+0,
+1,
+0
+],
+[
+1,
+0,
+0,
+0,
+1
+]
+]
+basic.forever(function () {
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            if (list[y][x] == 1) {
+                led.plot(x, y)
+            } else {
+                led.unplot(x, y)
+            }
+        }
+    }
+    if (_type == 1) {
+        for (let y = 0; y <= 4; y++) {
+            item = list[y]
+            item.unshift(item.pop())
+        }
+        basic.pause(100)
+    } else if (_type == 2) {
+        for (let y = 0; y <= 4; y++) {
+            item = list[y]
+            item.push(item.shift())
+        }
+        basic.pause(100)
+    } else if (_type == 3) {
+        item.push(item.pop())
+        basic.pause(100)
+    } else if (_type == 4) {
+        item.push(item.pop())
+        basic.pause(100)
+    }
+})
